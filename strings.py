992dc341c094210b20cd6cffb1ecce6c8c113e5c
Corey Wang <corefire2004@gmail.com>
2022-04-06 17:55:54 -0400

    no error if end of slice is too big

--- strings.py
+++ strings.py
@@ -1,5 +1,6 @@
 s="0123"
-print(s[:2])
+print(s[:100])
 #second argument (denoting end of slice index) only goes up to the index of that number -1, both strings and lists
+#no error from the second argument if its larger than the index
 l=[0,1,2,3]
 print(l[:2])
