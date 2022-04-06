e7bc50924f9866c3eb0ac9b5a09eca535f3eb11d
Corey Wang <corefire2004@gmail.com>
2022-04-06 19:49:28 -0400

    new file

---
+++ specifiations.py
@@ -0,0 +1,14 @@
+def count_vowels(s):
+    """
+    Returns: number of vowels in a string
+
+    Vowels are a,e,i,o,u
+
+    Upper case and lower case both work
+
+    Examples:...
+
+    Paramter s: The string to check for vowels
+    Precondition: s is a string
+    """
+    pass
--- strings.py
+++ strings.py
@@ -1,24 +1,9 @@
-def count_vowels(s):
-    """
-    Returns: number of vowels in a string
-
-    Vowels are a,e,i,o,u
-
-    Upper case and lower case both work
-
-    Examples:...
-
-    Paramter s: The string to check for vowels
-    Precondition: s is a string
-    """
-    pass
-
 s="0123"
 print(s[1:-1])
 #second argument (denoting end of slice index) only goes up to the index of that number -1, both strings and lists
 #no error from the second argument if its larger than the index
-# l=[0,1,2,3]
-# print(l[:2])
-# escape="\\hello\'"
-# print(escape)
-# print(len(escape))
+l=[0,1,2,3]
+print(l[:2])
+escape="\\hello\'"
+print(escape)
+print(len(escape))
