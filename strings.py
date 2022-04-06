4468cb19090c1cb80e09dd3509259d4306537699
Corey Wang <corefire2004@gmail.com>
2022-04-06 19:48:51 -0400

    docstring

--- strings.py
+++ strings.py
@@ -1,3 +1,18 @@
+def count_vowels(s):
+    """
+    Returns: number of vowels in a string
+
+    Vowels are a,e,i,o,u
+
+    Upper case and lower case both work
+
+    Examples:...
+
+    Paramter s: The string to check for vowels
+    Precondition: s is a string
+    """
+    pass
+
 s="0123"
 print(s[1:-1])
 #second argument (denoting end of slice index) only goes up to the index of that number -1, both strings and lists
