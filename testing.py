9e7ae476fb6149109dfc3e5f54c5870a719cb2ec
Corey Wang <corefire2004@gmail.com>
2022-04-07 11:48:12 -0400

    example

--- testing.py
+++ testing.py
@@ -1,3 +1,11 @@
 #black box testing - look only at result
 
+def divmod(a,b):
+    return a//b,a%b
+
+a,b=divmod(7,3)
+print(a)
+print(b)
+
+
 #white box testing - Using print statements to locate exactly what the error is in a function
