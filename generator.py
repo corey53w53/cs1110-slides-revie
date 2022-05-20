0b7bcf29ee08960ef63f8cd081ab83d8b1712faf
Corey Wang <corefire2004@gmail.com>
2022-05-20 09:08:20 -0400

    takes input from user and adds and prints

--- generator.py
+++ generator.py
@@ -1,14 +1,19 @@
-def gen(l):
-    counter=0
-    while True:
-        yield l[counter%(len(l))]
-        counter+=1
+# def gen(l):
+#     counter=0
+#     while True:
+#         yield l[counter%(len(l))]
+#         counter+=1
+# a=gen([1,2,3,4])
 
-a=gen([1,2,3,4])
-print(next(a))
-print(next(a))
-print(next(a))
-print(next(a))
-print(next(a))
-print(next(a))
-print(next(a))
+def sum_input():
+    sum=0
+    i = (yield)
+    while True:
+        sum+=i
+        i = (yield sum)
+
+b=sum_input()
+next(b)
+while True:
+    i=input("Enter number to sum: ")
+    print(f'Current Sum: {b.send(int(i))}')
